34ee00f07cb10768da7eec024577579b3db51edf
Andy B <andybarnov@gmail.com>
2017-03-06 18:08:02 +0300

    add more tests

--- spec/or_sandbox_spec.rb
+++ spec/or_sandbox_spec.rb
@@ -10,7 +10,18 @@ RSpec.describe 'Handling interrogative structures' do
   it 'correctly determine lower case "ok"' do
     expect(interrogative_adj?(tagger.tag("Is she ok?"))).to be_truthy
   end
-  it 'correctly change person from first to second' do
-    expect(interrogative_adj?(tagger.tag("am I fine"))).to eq "you are fine"
+
+  # That .drop(1) thing completely sucks, but I have to decide where do I get rid of it
+
+  it 'correctly inverts structure and changes person from first to second' do
+    expect(invert_interrogative_adj(tagger.tag("am I fine").drop(1))).to eq "you are fine"
+  end
+
+  it 'correctly inverts structure and keeps the proverb' do
+    expect(invert_interrogative_adj(tagger.tag("is he nice").drop(1))).to eq "he is nice"
+  end
+
+  it 'correctly inverts structure and keeps the proper noun' do
+    expect(invert_interrogative_adj(tagger.tag("is Wendy fine").drop(1))).to eq "Wendy is fine"
   end
 end
